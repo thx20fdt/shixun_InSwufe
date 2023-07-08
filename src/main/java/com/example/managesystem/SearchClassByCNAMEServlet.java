f0a6645a850e21565f2b9f7ba4df497a3698a082
tanghao <1930157993@qq.com>
2023-07-08 17:37:16 +0800

    添加两个分别按照CNAME和TNAME查询课程的Servlet，添加一个选择课程的jsp和一个处理选课请求的servlet，更改了一点主界面

---
+++ src/main/java/com/example/managesystem/SearchClassByCNAMEServlet.java
@@ -0,0 +1,54 @@
+package com.example.managesystem;
+
+import com.example.managesystem.course.course;
+import com.example.managesystem.db.DBUtil;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "SearchClassByCNAMEServlet", value = "/SearchClassByCNAMEServlet")
+public class SearchClassByCNAMEServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+        String CNAME=(String) request.getParameter("CNAME");
+        List<course> courseList = new ArrayList<>();
+        try {
+            Connection con = DBUtil.getConnection();
+            String sql = "select Class.CID ,Class.CNAME,Teacher.NAME,Class.CLASSTIME from Class,Teacher where Class.TID = Teacher.TID and Class.CNAME="+"'"+CNAME+"'";
+            Statement stmt =  con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                course c = new course();
+                c.setCID(rs.getString(1));
+                c.setCNAME(rs.getString(2));
+                c.setTNAME(rs.getString(3));
+                c.setCLASSTIME(rs.getString(4));
+                courseList.add(c);
+
+            }
+            DBUtil.closeConnection(con);
+            stmt.close();
+            rs.close();
+            request.setAttribute("courses",courseList);
+            request.getRequestDispatcher("SelectClass.jsp").forward(request,response);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
